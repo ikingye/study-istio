6f0af7e2f6424935a313e60a9f6d1ac7e23fd34b
yewang <yewang@baidu.com>
2020-11-29 16:49:23 +0800

    rebuilding site Sun Nov 29 16:49:20 CST 2020

--- en.search.min.5f7337f63be4c5f55ddd2c7b593e134bbf53a6ce20d9b7dca65a6f71ab2d93e7.js
+++ en.search.min.5f7337f63be4c5f55ddd2c7b593e134bbf53a6ce20d9b7dca65a6f71ab2d93e7.js
@@ -3,7 +3,7 @@ input.addEventListener('focus',init);input.addEventListener('keyup',search);docu
 const characterPressed=String.fromCharCode(event.charCode);if(!isHotkey(characterPressed)){return;}
 input.focus();event.preventDefault();}
 function isHotkey(character){const dataHotkeys=input.getAttribute('data-hotkeys')||'';return dataHotkeys.indexOf(character)>=0;}
-function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/study-istio/flexsearch.min.js');loadScript('/study-istio/en.search-data.min.8bdbef4a994cc5901d7fe22a3a33d109b93a5a0136c99c41454b874db39b6035.js',function(){input.required=false;search();});}
+function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/study-istio/flexsearch.min.js');loadScript('/study-istio/en.search-data.min.db28690a5f2d1cdb1d78809adcfa4ece64319c7c7f97316c8c579942a4b2e098.js',function(){input.required=false;search();});}
 function search(){while(results.firstChild){results.removeChild(results.firstChild);}
 if(!input.value){return;}
 const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=element('<li><a href></a><small></small></li>');const a=li.querySelector('a'),small=li.querySelector('small');a.href=page.href;a.textContent=page.title;small.textContent=page.section;results.appendChild(li);});}
